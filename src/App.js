b94343ad755098d041888fe7470e978b07e574e7
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-07 23:08:05 +0600

    setDisplayName

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Button, Form } from 'react-bootstrap';
@@ -10,11 +10,17 @@ import { useState } from 'react';
 const auth = getAuth(app)
 
 function App() {
+  const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [pass, setPass] = useState('')
   const [error, setError] = useState('')
   const [validated, setValidated] = useState(false);
   const [register, setRegistar] = useState(false)
+
+  const handelName = (event) => {
+    setName(event.target.value)
+
+  }
 
   const handelRegister = (event) => {
     setRegistar(event.target.checked)
@@ -62,6 +68,8 @@ function App() {
           console.log('register success');
           setEmail('')
           setPass('')
+          verifyEmail()
+          setuserName()
         })
         .catch(err => {
           console.log(err)
@@ -74,6 +82,39 @@ function App() {
 
     event.preventDefault();
   };
+  const forgatePassword = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log('password reset email send');
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  const setuserName = () => {
+    updateProfile(auth.currentUser, {
+      displayName: name
+    })
+      .then(() => {
+        console.log('updated');
+      })
+      .catch(err => {
+        setError(err.message)
+      })
+  }
+
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser)
+      .then(res => {
+        console.log('verifide link send');
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+  }
+
+
 
 
 
@@ -84,7 +125,11 @@ function App() {
       <h1 className='text-primary'>Please {register ? 'LogIn' : 'Register'} !!</h1>
 
       <Form noValidate validated={validated} onSubmit={hanelformSubmit}>
-
+        {(!register) &&
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control onBlur={handelName} type="text" placeholder="Your Name" required />
+          </Form.Group>}
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
@@ -111,6 +156,7 @@ function App() {
         <Button variant="primary" type="submit">
           {register ? 'LogIn' : 'Register'}
         </Button>
+        <Button onClick={forgatePassword} variant="link">Forgate Password</Button>
       </Form>
     </div>
   );
